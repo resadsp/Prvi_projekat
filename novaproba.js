4cf4f3cd4045385161d1bb78916d52fcf5c024d0
resadsp <resadspahovic94@gmail.com>
2023-09-20 15:57:04 +0200

    first commit

---
+++ novaproba.js
@@ -0,0 +1,10 @@
+
+
+function posaljipodatke(){
+    document.write ("Ime i prezime: MAIDA BALTIC SPAHOVIC <br>")
+    document.write ("Datum rodjenja: 07.02.1993 <br>")
+    document.write ("Bracni status: Udata <br>")
+    document.write ("JMBG: 0702993788915 <br>")
+    document.write ("...............")
+}
+
